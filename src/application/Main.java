b9216f6dc128129ebfb5325eeba789048c0eea1d
Kristian Trandem <kkrorkk@gmail.com>
2017-02-14 15:19:38 +0100

    static view finished

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,18 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.BorderPane;
-
-// lol
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			GridPane page = (GridPane) FXMLLoader.load(Main.class.getResource("form.fxml"));
+			Scene scene = new Scene(page,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
