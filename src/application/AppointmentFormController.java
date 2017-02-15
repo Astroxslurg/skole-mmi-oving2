89c1526f21a79bdd55783bba5ee24465049950fb
christholest <christ.holest@outlook.com>
2017-02-15 16:26:12 +0100

    added regex check for startTime

--- src/application/AppointmentFormController.java
+++ src/application/AppointmentFormController.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -22,6 +26,27 @@ public class AppointmentFormController {
 	void checkBoxChange(Event event) {
 		antReservations.setDisable(!repetitionCheckbox.isSelected());
 		freqReservations.setDisable(!repetitionCheckbox.isSelected());
+	}
+	
+	@FXML
+	void startTimeChange(Event event) {
+		//String currentTextInField = startTime.getText();
+		//if(currentTextInField.length() != 5) {
+		//	startTime.setStyle("-fx-control-inner-background: #ff0000");
+		//} else {
+		//startTime.setStyle("-fx-control-inner-background: #ffffff");
+		//}
+		//System.out.println(startTime.getText());
+		
+		String currentTextInField = startTime.getText();
+		String pattern = "\\d\\d:\\d\\d";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(currentTextInField);
+		if (!m.matches()) {
+			startTime.setStyle("-fx-control-inner-background: #ff0000");
+		} else {
+			startTime.setStyle("-fx-control-inner-background: #ffffff");
+		}
 	}
 	
 }
