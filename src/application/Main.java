35dc5646147bd975eaf53d7e431e7f7258509a00
Kristian Trandem <kkrorkk@gmail.com>
2017-02-15 14:17:34 +0100

    add controller object in main

--- src/application/Main.java
+++ src/application/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		Appointment appointment = new Appointment();
+		AppointmentFormController controller = new AppointmentFormController();
 		launch(args);
 	}
 }
