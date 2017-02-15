43103c74c445ebc98a8b793a92f1a47d09b7e206
christholest <christ.holest@outlook.com>
2017-02-15 13:39:37 +0100

    Renaming form.fxml to AppointmentForm.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane page = (GridPane) FXMLLoader.load(Main.class.getResource("form.fxml"));
+			GridPane page = (GridPane) FXMLLoader.load(Main.class.getResource("AppointmentForm.fxml"));
 			Scene scene = new Scene(page,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
