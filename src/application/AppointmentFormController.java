1ffe79e1cf72a65ab6dcee1985a13470636c4665
Kristian Trandem <kkrorkk@gmail.com>
2017-02-15 16:18:31 +0100

    add date validation

--- src/application/AppointmentFormController.java
+++ src/application/AppointmentFormController.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.time.LocalDate;
+
+import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -23,5 +26,16 @@ public class AppointmentFormController {
 		antReservations.setDisable(!repetitionCheckbox.isSelected());
 		freqReservations.setDisable(!repetitionCheckbox.isSelected());
 	}
+	
+	@FXML
+	void dateFieldChange(Event event){
+		LocalDate selectedDate = dateField.getValue();
+		LocalDate currentDate = LocalDate.now();
+		if (selectedDate.isAfter(currentDate)) {
+			dateField.setStyle("-fx-control-inner-background: #fff");
+		} else {
+			dateField.setStyle("-fx-control-inner-background: #f55");
+		}
+	}	
 	
 }
